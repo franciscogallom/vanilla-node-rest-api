81ad51bc06555e6c14dd748af07fc34ffac531a1
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-01-05 17:15:55 -0300

    Simple routing.

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const http = require('http')
 const products = require('./data/products.json')
 
 const server = http.createServer((req, res) => {
-    res.writeHeader(200, { 'Content-Type': 'aplication/json' })
-    res.end(JSON.stringify(products))
+    if(req.url === '/api/products' && req.method === 'GET'){
+        res.writeHeader(200, { 'Content-Type': 'aplication/json' })
+        res.end(JSON.stringify(products))
+    } else {
+        res.writeHeader(404, { 'Content-Type': 'aplication/json' })
+        res.end(JSON.stringify({ message: 'Route Not Found :('}))
+    }
 })
 
 const PORT = process.env.PORT || 3000
